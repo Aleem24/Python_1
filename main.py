1d0038d3b9f8c65998ceb6903ce7048deb272fe8
Aleem24 <md.samiulaleem16@gmail.com>
2025-01-11 19:42:33 +0600

    Python DataType and String Slicing

---
+++ main.py
@@ -0,0 +1,24 @@
+# Data Types
+name = "Aleem"
+age = 10
+is_student= True
+weight = 28.7
+
+print(type(name))
+print(type(age))
+print(type(is_student))
+print(type(weight))
+
+
+# Type Conversion and user input
+
+user_input1 = input("Enter user_input1:")
+user_input2 = input("Enter user_input2:")
+
+print(int(float(user_input1)+int(float(user_input2))))
+
+# String Slicing
+text = "I love my family"
+
+slicing = text[0:6]
+print(slicing)
