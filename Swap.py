41e7249c5489cb58e252dbde7d72b4dc37d36cf2
Aleem24 <md.samiulaleem16@gmail.com>
2025-01-13 18:09:49 +0600

    Swapping for Homework

---
+++ Swap.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter Number 1:"))
+num2 = int(input("Enter Number 2:"))
+
+temp = num1
+print(temp,num1)
+num1 = num2
+print(num1,num2)
+num2 = temp
+print(num2,temp)
+
+print(num1)
+print(num2)
+print(temp)
